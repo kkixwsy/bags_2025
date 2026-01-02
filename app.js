0e7cfbd2ad146d0fb48f8ba5bdb024e68e35b8c1
Diana <erediana88@gmail.com>
2026-01-03 01:51:17 +0300

    bags_9_1

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/bags_2025')
+var session = require("express-session")
+
 
 
 var indexRouter = require('./routes/index');
@@ -23,6 +25,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+ secret: "ThreeCats",
+ cookie:{maxAge:60*1000},
+ proxy: true,
+ resave: true,
+ saveUninitialized: true
+}))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
