bc698a29e827fd713680207a9b834ae3c7cad120
Diana <erediana88@gmail.com>
2026-01-03 01:35:10 +0300

    bags_8_4

--- routes/bags.js
+++ routes/bags.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var Bag = require('../models/bag').Bag;
+var Bags = require('../models/bag').Bags;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -9,7 +9,7 @@ router.get('/', function(req, res, next) {
 
 /* Страница котов */
 router.get("/:nick", async function(req, res, next) {
-   var bags = await Bag.find({nick: req.params.nick});
+   var bags = await Bags.find({nick: req.params.nick});
    console.log(bags)
    if(!bags.length) return next(new Error("Нет такого котенка в мультфильме Три кота"))
        var bag = bags[0];
--- routes/index.js
+++ routes/index.js
@@ -6,29 +6,5 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-/* Страница Birkin*/
-router.get('/birkin', function(req, res, next) {
-    res.render('bag', {
-        title: "Биркин",
-        picture: "images/birkin.jpg",
-        desc: "Сумка Биркин от Hermes — символ роскоши и эксклюзивности. Она известна своим минималистичным дизайном и высокой ценой. Каждая сумка создается вручную, с использованием качественных материалов."
-    });
-});
-/* Страница Longchamp*/
-router.get('/longchamp', function(req, res, next) {
-    res.render('bag', {
-        title: "Лонгчемп",
-        picture: "images/longchamp.jpg",
-        desc: "Сумка Лонгчемп, особенно модель Le Pliage, известна своей легкостью и компактностью. Она удобна для повседневного использования и представлена в различных цветах и размерах, сохраняя стиль и функциональность."
-    });
-});
-/* Страница Chanel*/
-router.get('/chanel', function(req, res, next) {
-    res.render('bag', {
-        title: "Шанель",
-        picture: "images/chanel.jpg",
-        desc: "Сумка Шанель — это элегантность и стиль. Модели с цепочкой и классическим лого стали культовыми. Эти сумки часто ассоциируются с французским шиком и высокой модой."
-    });
-});
 
 module.exports = router;
