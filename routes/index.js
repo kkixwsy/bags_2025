b0532234bde5c0775f01f1923a0d37acbbf9b1c3
Diana <erediana88@gmail.com>
2026-01-03 03:09:09 +0300

    bags_9_3

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
- res.cookie('greeting', 'Hi!!!').render('index', { title: 'Express' });
+ req.session.greeting = "Hi!!!";
+ res.render('index', { title: 'Express' });
+});
+router.get('/test-session', (req, res) => {
+  req.session.greeting = 'Привет от сессии!';  // ← это заставит сохранить сессию
+  req.session.views = (req.session.views || 0) + 1;
+
+  res.send(`
+    <h1>Тест сессии</h1>
+    <p>Приветствие: ${req.session.greeting}</p>
+    <p>Посещений этой страницы: ${req.session.views}</p>
+    <p><a href="/">На главную</a></p>
+  `);
 });
 
 module.exports = router;
