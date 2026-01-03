7f5b89ac0da24f7dfc9464f27fde5da769d3f818
Diana <erediana88@gmail.com>
2026-01-03 22:56:34 +0300

    bags_10_8

--- routes/bags.js
+++ routes/bags.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var Bags = require('../models/bag').Bags;
+var checkAuth = require("../middlewares/checkAuth.js");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('Новый маршрутизатор, для маршрутов, начинающихся с bags');
 });
 
-/* Страница котов */
-router.get("/:nick", async function(req, res, next) {
+/* Страница сумок */
+router.get("/:nick", checkAuth, async function(req, res, next) {
    var bags = await Bags.find({nick: req.params.nick});
    console.log(bags)
    if(!bags.length) return next(new Error("Нет такого котенка в мультфильме Три кота"))
